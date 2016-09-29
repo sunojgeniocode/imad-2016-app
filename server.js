732482227348604d17014b6a0b6f3b104cfae1a7
sunojgeniocode <sunojbangalore@gmail.com>
2016-09-29 15:25:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get('/article-two', function(req, res){
 app.get('/article-three', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
